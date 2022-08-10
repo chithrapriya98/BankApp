121fd7a499470211f6eb9b8169ee0a0f2743076e
chithrapriya98 <110228647+chithrapriya98@users.noreply.github.com>
2022-08-10 10:28:08 +0530

    Delete Bankapppro directory

--- Bankapppro/BankApplication1/src/main/java/com/bank/app/Repository/TransactionRepositroy.java
+++
@@ -1,9 +0,0 @@
-package com.bank.app.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bank.app.Entity.Transaction;
-
-public interface TransactionRepositroy extends JpaRepository<Transaction, Long>{
-
-}
